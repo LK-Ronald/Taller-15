cbb9aa8e13114d0f5a7b1764f51bb840aaf4d6b8
LkRonald <rpereirac@unicartagena.co>
2025-11-05 15:06:20 -0500

    feat: se agrego la clase MostrarInfo con el SRP

---
+++ Ejecicio1/src/main/java/SRP/MostrarInfo.java
@@ -0,0 +1,10 @@
+package SRP;
+
+public class MostrarInfo {
+
+    public void mostrarLibro(String titulo, String autor, String precio) {
+        System.out.println("Titulo: " + titulo
+                + " Autor: " + autor
+                + " Precio: $" + precio);
+    }
+}
